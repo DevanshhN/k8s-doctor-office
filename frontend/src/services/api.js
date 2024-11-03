b8d0b65ac93b2cafa40a8e127904caec33980638
Ajay Sengar <ajay.senger2@gmail.com>
2024-11-03 13:37:52 -0500

    Fixed the LocalHost issue in frontend.2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://backend-service:3000/api';
+const API_URL = 'https://backend-service:3000/api';
 
 
 export const getAppointments = () => axios.get(`${API_URL}/appointments`);
